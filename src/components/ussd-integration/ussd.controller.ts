9f9bb9a95b95078a367ababeedb6e81be5c5d1d8
Wambua Musalu <musaluwambua@gmail.com>
2023-04-26 08:38:22 +0300

    accepting multiple parameters

--- src/components/ussd-integration/ussd.controller.ts
+++ src/components/ussd-integration/ussd.controller.ts
@@ -1,28 +1,25 @@
-import { Body, Controller, Get, Post, Req } from "@nestjs/common";
-import { UssdService } from "./ussd.service";
-import { Request } from "express";
-import { UssdBody } from "./ussd.dto";
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { UssdService } from './ussd.service';
+import { Request } from 'express';
+import { UssdBody } from './ussd.dto';
 
 @Controller('ussd')
 export class UssdController {
-    constructor(private ussdService: UssdService) {}
+  constructor(private ussdService: UssdService) {}
 
-    @Post('post')
-    getReqBody(@Body() dto: UssdBody) {
-        console.log(dto)
-        if(dto.text == '1'){
-                return this.ussdService.recordIncident()
-            }
-        else if ("1*1"){
-            return 'its is good to be home'
-        }
-        else{
-            return ' CON Welcome to Africa is Home \n 1. Would you Like to report an Incident'
-        }
-        
+  @Post('post')
+  getReqBody(@Body() dto: UssdBody) {
+    console.log(dto);
+    if (dto.text == '1') {
+      return this.ussdService.recordIncident();
+    } else if (dto.text == '1*1') {
+      return 'CON its is good to be home';
+    } else {
+      return ' CON Welcome to Africa is Home \n 1. Would you Like to report an Incident';
     }
-    @Get('resp')
-    respond(@Req() req: Request) {
-        console.log(req)
-    }
+  }
+  @Get('resp')
+  respond(@Req() req: Request) {
+    console.log(req);
+  }
 }
