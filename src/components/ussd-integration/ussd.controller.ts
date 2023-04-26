f3ce9e06dbbd014baff723502f86b5d64428761d
Wambua Musalu <musaluwambua@gmail.com>
2023-04-26 16:27:27 +0300

    attempt on getting user input

--- src/components/ussd-integration/ussd.controller.ts
+++ src/components/ussd-integration/ussd.controller.ts
@@ -15,7 +15,7 @@ class UssdController {
     } else if (dto.text == '1') {
         return ' CON Sasa, unaitwa nani?/ Hi, what\'s your name';
     } 
-    else if (dto.text == '1'+'*[a-zA-Z]'){
+    else if (dto.text == '1'+/^[A-Za-z]+$/){
         return ' CON Sasa, unaitwa nani?/ Hi, what\'s your name';
     }
     else if (dto.text == '2') {
