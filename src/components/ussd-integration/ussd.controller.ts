bb07dbb652c8f95151180b56fdc5f97d15da64fa
Wambua Musalu <musaluwambua@gmail.com>
2023-04-26 12:44:16 +0300

    ussd landing page

--- src/components/ussd-integration/ussd.controller.ts
+++ src/components/ussd-integration/ussd.controller.ts
@@ -13,9 +13,9 @@ class UssdController {
     if (dto.text == '1') {
         return ' CON Welcome to Africa is Home \n 1. Would you Like to report an Incident';
     } else if (dto.text == 'FGM') {
-      return true; //this.ussdService.recordIncident(dto);
+      return 'CON FGM landing'
     } else if (dto.text == 'Rape') {
-      return true; //this.ussdService.recordIncident(dto);
+      return 'CON Rape landing'
     } else {
       return ' END Invalid input';
     }
