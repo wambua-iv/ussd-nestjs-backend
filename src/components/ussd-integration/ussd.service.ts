57863b5956971060453f9d8d972163365d3b82e9
Wambua Musalu <musaluwambua@gmail.com>
2023-04-26 10:24:54 +0300

    extend routing functionality

--- src/components/ussd-integration/ussd.service.ts
+++ src/components/ussd-integration/ussd.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { DatabaseService } from "../database/Database.service";
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/Database.service';
+import UssdMenu from 'ussd-builder';
+import { UssdBody } from './ussd.dto';
 
 @Injectable()
 export class UssdService {
-    constructor(private database : DatabaseService) {}
-    
-    recordIncident() {
-        return 'CON Are you reporting for self'
+  constructor(
+    private database: DatabaseService,
+    private ussdHandler: UssdMenu,
+  ) {}
+
+  recordIncident(dto: UssdBody) {
+    this.ussdHandler.startState({
+      run: () => {
+        this.ussdHandler.con('What case are you reporting \n 1.FGM \n 2. Rape');
+      },
+      next: {
+        '1': 'FGM',
+        '2': 'Rape'
     }
+    });
+
+    this.ussdHandler.state('FGM', {
+        run: () => {}
+    })
+  }
 }
